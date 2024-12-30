c9e0599ae6152c02e9b7042db7bfd6674781bd84
Brfasl <berfin1884@gmail.com>
2024-12-30 09:58:59 +0300

    Names have been corrected. A new item has been created.

---
+++ Clothing and Size Analysis Automation/RibbonForm1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace Login_And_Register_Page
+{
+    public partial class RibbonForm1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public RibbonForm1()
+        {
+            InitializeComponent();
+        }
+    }
+}
